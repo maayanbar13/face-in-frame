048c48d49b0d7eca04a97850d8ce7e5335fe8483
מעיין בר <maayanbar13@gmail.com>
2019-10-13 16:07:55 +0300

    removed everything for CR

--- src/components/tile/layout/onTileContentLayout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const OnTileContentLayout = (props) => {
-    return (
-        <div className="centralized">
-            {props.children}
-        </div>
-    );
-};
